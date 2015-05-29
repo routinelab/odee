51b6d04590a3186f4070da4380aca51c1523e21a
Jeff Yang <routinelab@gmail.com>
2015-05-30 02:10:33 +0900

    Set default application structure

---
+++ config/config.js
@@ -0,0 +1,5 @@
+// Invoke 'strict' Javascript mode
+'use strict';
+
+// Load the correct configuration file according to the 'NODE_ENV' variable
+module.exports = require('./env/' + process.env.NODE_ENV);
